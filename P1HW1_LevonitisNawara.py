7fd4ff82725ddc31f50e9d62ef3ec8384e0834ed
levonitn8052 <159399463+levonitn8052@users.noreply.github.com>
2024-02-11 22:14:33 -0500

    Add files via upload
    P1HW1 assignment

---
+++ P1HW1_LevonitisNawara.py
@@ -0,0 +1,20 @@
+# Nawara Levonitis
+
+# 02/11/2024
+
+# P1HW1
+
+# It collects information from user, processes information collected and display results to user.
+
+Number_one = int(input())
+print("Enter integer:", Number_one)
+print("You entered:", Number_one)
+print(Number_one, "squard is", Number_one*Number_one)
+print("And", Number_one, "cubed is", Number_one*Number_one*Number_one, "!!")
+
+Number_two = int(input())
+print("Enter another integer:", Number_two)
+print(Number_one, "+", Number_two, "is", Number_one+Number_two)
+print(Number_one, "*", Number_two, "is", Number_one*Number_two)
+ 
+ 
