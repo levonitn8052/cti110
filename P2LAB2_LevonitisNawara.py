e6c639762d46a30bd50af294e9c5ef7f3faaa8fc
levonitn8052 <159399463+levonitn8052@users.noreply.github.com>
2024-03-02 00:15:29 -0500

    Add files via upload
    P2LAB2. Simple statistics.

---
+++ P2LAB2_LevonitisNawara.py
@@ -0,0 +1,20 @@
+# Nawara Levonitis
+
+# 3/1/2024
+
+# P2LAB1
+
+# mathematical calculations: Simple statistics
+
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+#sum of products and their average
+product = num1 * num2 * num3 * num4
+average = (num1 + num2 + num3 + num4) / 4
+
+#output the values
+print('{:.0f} {:.0f}'.format(product,average))
+print('{:.3f} {:.3f}'.format(product,average))
